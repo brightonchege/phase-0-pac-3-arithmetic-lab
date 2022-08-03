9eeb7f9497ef6a160f5f2e466d1a635d64adda12
brightonchege <brightonspak@gmail.com>
2022-08-03 20:53:41 +0300

    complete

---
+++ index.js
@@ -0,0 +1,48 @@
+function add( a,b){
+    return a + b 
+}
+add(10,20)
+function subtract(a,b){
+    return a - b
+}
+subtract(10,5)
+
+
+function multiply(a,b){
+    return a * b
+}
+multiply(10,5)
+
+
+function divide(a,b){
+    return a / b
+}
+divide(10,5)
+
+
+
+
+function increment(n){
+n = ++n
+return n
+}
+increment(n)
+
+
+function decrement(n){
+    n = --n
+    return n
+}
+decrement(n)
+
+function makeInt(n){
+    return parseInt(n,10)
+    }
+makeInt(n)
+
+
+    function preserveDecimal(n){
+    return parseFloat(n)
+
+    }
+    preserveDecimal(n)
